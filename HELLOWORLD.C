194094bde2e67189e673733d5fd1e4fed2eaa76d
Shubham Jadhav <111007875+ShubhamJadhav-github@users.noreply.github.com>
2023-02-28 12:29:35 +0530

    Update HELLOWORLD.C

--- HELLOWORLD.C
+++ HELLOWORLD.C
@@ -1,12 +1,15 @@
 //Program To Print a String "HelloWorld"
 
-#include<stdio.h>       //Preprocessor directive to include a standard input output hearder file
 
-int main()               //main function with return as an int
+#include<stdio.h>       //Preprocessor directive to include a standard input - output hearder file
+#include<conio.h>       //Preprocessor directive to include a console input - output hearder file
+
+int main()              //main function with return as an int
 {
-  
+
   //Body of main Function
-  printf("HelloWorld"); //calling function printf() by passing a string "HelloWorld" as an argument
+  printf("Hello World");//calling function printf() by passing a string "HelloWorld" as an argument
+  getch();              //Here the program execution will be paused Because getch() will require an input from keyboard (To hold the screen, so that user can see the output)
   return 0;             //returning an integer value zero
-  
+        
 }
