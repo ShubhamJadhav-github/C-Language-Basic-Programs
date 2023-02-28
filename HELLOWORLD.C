ec575cac41988e725336a17fb362659c41b9834e
Shubham Jadhav <111007875+ShubhamJadhav-github@users.noreply.github.com>
2023-02-28 12:01:23 +0530

    Create HELLOWORLD.C

---
+++ HELLOWORLD.C
@@ -0,0 +1,7 @@
+#include<stdio.h>       //Preprocessor directive to include a standard input output hearder file
+int main()               //main function with return as an int
+{
+  //Body of main Function
+  printf("HelloWorld"); //calling function printf() by passing a string "HelloWorld" as an argument
+  return 0;             //returning an integer value zero
+}
