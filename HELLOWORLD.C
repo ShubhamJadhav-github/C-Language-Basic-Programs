a67824df833d18fba21ec7e93408bf71368d4b88
Shubham Jadhav <111007875+ShubhamJadhav-github@users.noreply.github.com>
2023-02-28 12:14:50 +0530

    Update HELLOWORLD.C
    Program in C To Print a String "HelloWorld"

--- HELLOWORLD.C
+++ HELLOWORLD.C
@@ -1,7 +1,12 @@
+//Program To Print a String "HelloWorld"
+
 #include<stdio.h>       //Preprocessor directive to include a standard input output hearder file
+
 int main()               //main function with return as an int
 {
+  
   //Body of main Function
   printf("HelloWorld"); //calling function printf() by passing a string "HelloWorld" as an argument
   return 0;             //returning an integer value zero
+  
 }
