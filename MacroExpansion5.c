403ca03e6f27a59d51d5f2e044e305257129e083
Shubham Jadhav <shubhamjadhav0806@gmail.com>
2024-02-04 11:21:19 +0530

    Create MacroExpansion5.c

---
+++ MacroExpansion5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#define PRINT_HELLO_TEN_TIMES for(i=0;i<10;i++)\
+				printf("\nHello");
+int main()
+{
+	int i;
+	clrscr();
+	PRINT_HELLO_TEN_TIMES;
+	getch();
+	return 0;
+}
